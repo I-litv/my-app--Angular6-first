c45d53616bc84e9776f7d814ee23f25e260fba05
Igor <dovid1989@gmail.com>
2018-10-04 14:39:53 +0300

    Update
    0.0.01

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+})
+
+export class ServerComponent implements OnInit {
+  serverId: 10;
+  serverStatus: 'online';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
